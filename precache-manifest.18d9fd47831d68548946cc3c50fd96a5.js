bbc77fff0d8477f796a319e33d748a4f21f2c9e0
nguyenhung1512 <50404761+nguyenhung1512@users.noreply.github.com>
2019-05-08 18:02:10 +0700

    Add files via upload

---
+++ precache-manifest.18d9fd47831d68548946cc3c50fd96a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9905da36f26871386cc2",
+    "url": "/static/css/main.790d4c46.chunk.css"
+  },
+  {
+    "revision": "9905da36f26871386cc2",
+    "url": "/static/js/main.41cf6c9e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a85151cca8c991b8fd3",
+    "url": "/static/js/2.e82a78c9.chunk.js"
+  },
+  {
+    "revision": "d77654c1a239068e62f36f1d74eb0355",
+    "url": "/index.html"
+  }
+];
